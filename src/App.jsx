0cd24d1e1fe51d855b21a3fdd42e8acbbff7eb18
Ismail Shaikh <91933697+coddyismail@users.noreply.github.com>
2025-09-28 00:48:22 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import home from "./Home.jsx";
+import Home from "./home.jsx";
 import EightDConverter from "./EightDConverter";
 
 export default function App() {
