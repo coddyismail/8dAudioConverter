c5e1d3edd5ca8a3fb6f25398dfadf7483978c03d
Ismail Shaikh <91933697+coddyismail@users.noreply.github.com>
2025-09-28 01:00:18 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import home from "./home.jsx";
+import Home from "./src/home.jsx";
 import EightDConverter from "./EightDConverter";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/convert" element={<EightDConverter />} />
       </Routes>
     </Router>
